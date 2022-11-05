92678926e2f61394027fa4d722b12500ec3e37e5
DanyloSydorchuk <79981677+DanyloSydorchuk@users.noreply.github.com>
2022-11-05 17:16:58 +0100

    Drumkit

--- Lab3Drumkit/drumkit.js
+++ Lab3Drumkit/drumkit.js
@@ -1,37 +1,17 @@
 document.addEventListener('keypress', onKeyPress)
 
-let music = {
-    sound: null,
-    time: null
-}
 let startTime = 0
 let stopTime = 0
 let isRecording = 0
+let count = 1
+let record = []
+let sound1 = []
+let sound2 = []
+let sound3 = []
+let sound4 = []
 
 document.querySelector('#startRecord').addEventListener('click',recordStart)
 document.querySelector('#stopRecord').addEventListener('click',recordStop)
-
-// const sound= {
-//     's1' : document.querySelector('#s1'),
-//     's2' : document.querySelector('#s2')
-// }
-
-// function playSound(event){
-//     switch(event.key){
-//         case 'a':
-//             // console.log(event)
-//             sound.s1.currentTime = 0
-//             sound.s1.play()
-//         case 's':
-//             // console.log(event)
-//             sound.s2.currentTime = 0
-//             sound.s2.play()
-//     }
-    
-// }
-
-
-// const licznik = 0
 
 const keyToSound= {
     'a' : document.querySelector('#s1'),
@@ -45,12 +25,55 @@ const keyToSound= {
     'g' : document.querySelector('#s9')
 }
 
+document.querySelector('#sound1').addEventListener('click',() => {play(sound1)})
+document.querySelector('#sound2').addEventListener('click',() => {play(sound2)})
+document.querySelector('#sound3').addEventListener('click',() => {play(sound3)})
+document.querySelector('#sound4').addEventListener('click',() => {play(sound4)})
+document.querySelector('#soundAll').addEventListener('click',() => {
+    play(sound1)
+    play(sound2)
+    play(sound3)
+    play(sound4)
+})
+
+document.querySelector('#play').addEventListener('click',() => {
+    if (document.querySelector('#sound1check').checked)
+    {
+        play(sound1)
+    }
+    if (document.querySelector('#sound2check').checked)
+    {
+        play(sound2)
+    }
+    if (document.querySelector('#sound3check').checked)
+    {
+        play(sound3)
+    }
+    if (document.querySelector('#sound4check').checked)
+    {
+        play(sound4)
+    }
+})
+
+function play(tab){
+    for (let i = 1; i < tab.length; i++) {
+        //console.log(tab[i][0] + ':' + tab[i][1])  
+        timeout(tab[i][0],tab[i][1])    
+    }
+}
+
+function timeout(sound, time){
+    setTimeout(function(){
+        playSound(sound)
+    },time)
+}
+
 function onKeyPress(event){
     const sound = keyToSound[event.key]
-    console.log(sound)
+    console.log(event.key)
     playSound(sound)
-    if(isRecording=1){
-        recording(sound,getTime())
+    if(isRecording==1){
+        recording(sound,getTime()-startTime)
     }
 }
 
@@ -61,17 +84,9 @@ function playSound(sound){
 
 function getTime(){
     const time = new Date().getTime()
-    console.log(time)
+    console.log(time-startTime)
     return time
 }
-
-// function record(soundR,timeR){
-//     const start = getTime()
-//     new music ={
-//         sound:soundR,
-//         time:timeR
-//     }
-// }
 
 function recordStart(){
     startTime = getTime()
@@ -81,14 +96,29 @@ function recordStart(){
 function recordStop(){
     stopTime = getTime()
     isRecording = 0
-    console.log(music)
+    console.log(record)
+    if(sound1.length==0){
+        sound1=record
+    }
+    else if(sound2.length==0){
+        sound2=record
+    }
+    else if(sound3.length==0){
+        sound3=record
+    }
+    else{
+        sound4=record
+    }  
+    record=[]
+    startTime = 0
+    stopTime = 0
+    isRecording = 0
+    count = 1
 }
 
 function recording(soundR,timeR){
-    music = {
-        sound : soundR,
-        time : timeR-startTime
-    }
+    record[count]=[soundR,timeR]
+    count++
 }
 
 
