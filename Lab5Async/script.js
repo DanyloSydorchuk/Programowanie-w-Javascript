114336fe9fde00367a22b06e82af2f3eef2630ce
Danylo Sydorchuk <79981677+DanyloSydorchuk@users.noreply.github.com>
2022-11-27 14:11:41 +0100

    Lab5

---
+++ Lab5Async/script.js
@@ -0,0 +1,38 @@
+
+const a = [1,2,3,4,5,6,7,8,9,10]
+let sum=0
+
+
+
+const asyncAdd = async (a,b) => {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      return Promise.reject('Argumenty muszą mieć typ number!')
+    }
+    return new Promise((resolve, reject) => {
+      setTimeout(() =>{
+        resolve(a+b)
+      }, 100)
+    })
+  }
+
+  function separator(tab){
+    for(i=0;i<tab.length;i++){
+      // asyncAdd(tab[i],tab[i]).then(b=>sum = b)
+
+
+
+
+      if(i==0){
+        sum=tab[i]
+        console.log('1:'+sum)
+      }
+      else{
+        asyncAdd(sum,tab[i]).then((data)=>{
+          sum=data
+          console.log(data)
+        }).catch((e) => console.log(e))
+      }
+    }
+  }
+
+  separator(a)
